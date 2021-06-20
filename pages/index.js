3179b10d8914b3aadcb288814d9a3e9b76ff0c21
at1208 <amantiwari12081999@gmail.com>
2021-06-20 16:02:23 +0530

    fix: issues and improvements

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,6 @@ import AuthorCardSkeleton from '../components/Blog/Cards/authorCardSkeleton'
 import WbIncandescentIcon from '@material-ui/icons/WbIncandescent';
 
 import Layout from '../components/Layout';
-import { one_tap_login, authenticate, isAuth} from '../actions/auth';
 import { blog_list, author_list, trending_list } from '../actions/blog';
 import {random_categories } from '../actions/category';
 
@@ -23,7 +22,6 @@ import {random_categories } from '../actions/category';
 const Home = ({ largeBlogs, smallBlogs, mediumBlogs }) => {
   const [authors, setAuthors] = useState();
   const [trendingBlogs, setTrending] = useState();
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [categories, setCategories] = useState();
 
 
@@ -72,34 +70,27 @@ const Home = ({ largeBlogs, smallBlogs, mediumBlogs }) => {
   }, [])
 
 
-  const handleOnetapResponse = (response) => {
-  // const decodedToken = jwt_decode(response.credential)
-    one_tap_login({ googleToken: response.credential, domain: process.env.NEXT_PUBLIC_DOMAIN_ID })
-      .then(result => {
-        authenticate(result, () => {
-          setIsAuthenticated(true)
-        })
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }
-
-  useEffect(() => {
-    if(!isAuth()){
-      window.onload = function () {
-         google.accounts.id.initialize({
-           client_id:process.env.NEXT_PUBLIC_GOOGLE_CLIEND_ID,
-           callback: handleOnetapResponse
-         });
-         google.accounts.id.prompt();
-       }
-    }
-  }, [])
 
   function HeaderSEO(){
      return <Head>
-               <title>Home</title>
+               <title>{process.env.NEXT_PUBLIC_APP_NAME}</title>
+              <meta
+                  name="description"
+                  content="Includes all type of scientific information, theories and facts that are related to science and technology. Brain &amp; Behavior 2. Earth, Energy and Environment, Role of Science in Health Industry, Life and Non-living, Physics, Chemistry and Mathematical experiments,Space and Astronomy,Technology"
+              />
+              <link rel="canonical" href={`${process.env.NEXT_PUBLIC_DOMAIN_URL}`} />
+              <meta property="og:title" content={`Includes all type of scientific information, theories and facts that are related to science and technology. Brain &amp; Behavior 2. Earth, Energy and Environment, Role of Science in Health Industry, Life and Non-living, Physics, Chemistry and Mathematical experiments,Space and Astronomy,Technology | ${process.env.NEXT_PUBLIC_APP_NAME}`} />
+              <meta
+                  property="og:description"
+                  content="Includes all type of scientific information, theories and facts that are related to science and technology. Brain &amp; Behavior 2. Earth, Energy and Environment, Role of Science in Health Industry, Life and Non-living, Physics, Chemistry and Mathematical experiments,Space and Astronomy,Technology"
+              />
+              <meta property="og:type" content="website" />
+              <meta property="og:url" content={`${process.env.NEXT_PUBLIC_DOMAIN_URL}`} />
+              <meta property="og:site_name" content={`${process.env.NEXT_PUBLIC_APP_NAME}`} />
+              <meta property="og:image" content={`${process.env.NEXT_PUBLIC_DOMAIN_URL}/scientifly.svg`} />
+              <meta property="og:image:secure_url" content={`${process.env.NEXT_PUBLIC_DOMAIN_URL}/scientifly.svg`} />
+              <meta property="og:image:type" content="image/jpg" />
+              <meta property="fb:app_id" content={`${process.env.NEXT_PUBLIC_FB_APP_ID}`} />
             </Head>
    }
 
@@ -162,7 +153,7 @@ const Home = ({ largeBlogs, smallBlogs, mediumBlogs }) => {
   }
   return <>
            <HeaderSEO  />
-           <Layout isAuthenticated={isAuthenticated}>
+           <Layout>
               <div className="div-container mb-5">
                  <div className="row col">
                     <div className="col-md-4 col-sm-5 col-lg-4">
